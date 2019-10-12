05e6be25c4329c23c751be4ee19b42c440f94ea8
kauktus <55836097+kauktus@users.noreply.github.com>
2019-10-12 10:25:25 +0500

    Update askname.c

--- askname.c
+++ askname.c
@@ -3,6 +3,7 @@
 void askname(char *first, char *last)
 {
 printf(&quot;Enter your first name: &quot;);
+printf(&quot;Enter your first name: &quot;);
 fgets(first, 255, stdin);
 first[strlen(first)-1] = &#39;\0&#39;; /* remove the newline at the end */
 printf(&quot;Now enter your last name: &quot;);
